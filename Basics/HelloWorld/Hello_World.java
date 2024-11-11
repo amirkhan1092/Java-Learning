872a00f1ce7d71124fa6bf371224e3e1c07f3e84
amirkhan1092 <amirkhan1092@gmail.com>
2024-11-11 09:46:51 +0530

    multi line string in java

--- Basics/HelloWorld/Hello_World.java
+++ Basics/HelloWorld/Hello_World.java
@@ -4,5 +4,12 @@ public class Hello_World {
     
 public static void main(String[] args) {
     System.out.println("Hello, World!");
+
+    // multiline String text
+    String msg = String.format("""
+            Hello, Java
+            Are we ready to introduce multiline string
+            """);
+    System.out.println(msg);
 }
 }
