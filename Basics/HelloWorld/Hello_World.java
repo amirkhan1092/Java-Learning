96bc651c860581f284c77bd18ee8797f0ed01b38
amirkhan1092 <amirkhan1092@gmail.com>
2024-11-11 11:37:01 +0530

    hello world with format

--- Basics/HelloWorld/Hello_World.java
+++ Basics/HelloWorld/Hello_World.java
@@ -1,15 +1,26 @@
 package Basics.HelloWorld;
-
+import java.text.MessageFormat;
 public class Hello_World {
     
 public static void main(String[] args) {
     System.out.println("Hello, World!");
 
     // multiline String text
-    String msg = String.format("""
-            Hello, Java
-            Are we ready to introduce multiline string
-            """);
+    String msg;
+    msg = """
+        Can you help me to use multi_line String
+        in Java as the feature i found preveous in Python
+        i am so happy 
+        """;
+    // msg = String.format("""
+    //         Hello, Java
+    //         Are we ready to introduce multiline string
+    //         and it also hold my data %d
+    //         """, 10);
+    var v1 = 10;
+    var v2 = 20; 
+    // msg = "First Value is " + v1 + " Second value" + v2;
+    msg =  MessageFormat.format("First Value is {0}%nSecond value {1}", v1, v2);
     System.out.println(msg);
 }
 }
