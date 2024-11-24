1177766d9e94dba8d470a321d8f989825a82738d
amirkhan1092 <amirkhan1092@gmail.com>
2024-11-25 01:10:34 +0530

    number less upto 100

---
+++ Basics/Critical_Question/Numberless_Upto100.java
@@ -0,0 +1,11 @@
+package Basics.Critical_Question;
+
+public class Numberless_Upto100 {
+    public static void main(String[] args) {
+        int a = 'a' / 'a';
+        for(; a <= 'd'; a++)
+        {
+            System.out.println(a);
+        }
+    }
+}
