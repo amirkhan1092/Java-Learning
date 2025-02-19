3a1fdc5793d355736d93ba1e4579c98168c49e26
amirkhan1092 <amirkhan1092@gmail.com>
2025-02-19 18:49:10 +0530

    String questions

---
+++ StacksProblems/StringCompressDecode.java
@@ -0,0 +1,28 @@
+package StacksProblems;
+
+/*
+Write a program to give the following
+output for the given input
+Asked in ORACLE, facebook
+Example 1:
+Input: "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+Input: "3[a2[c]]"
+Output: "accaccacc"*/
+
+import java.util.Scanner;
+
+class StringCompressDecode{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.nextLine();
+
+
+
+
+        sc.close();
+    }
+}
+
+
